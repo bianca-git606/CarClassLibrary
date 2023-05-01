8f0c9ad9fb75eaafd6c7fa71c81e00ffc6948b0b
biancagit-606 <biancafiedalan31@yahoo.com>
2023-05-01 21:38:47 +1200

    Add project files.

---
+++ Car.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarClassLibrary
+{
+    public class Car
+    {   //get-set just means you can set and get the values
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public decimal Price { get; set; }
+
+        public Car() {
+
+            Make = "nothing yet";
+            Model = "nothing yet";
+            Price = 0.00M;
+        }
+
+        public Car(string make, string model, decimal price)
+        {
+            Make = make;
+            Model = model;
+            Price = price;
+        }
+        //prints a string when referencing an object
+        override public string ToString() 
+        {
+            return Make + "\t" + Model + "\t" + Price;
+        }
+    }
+}
---
+++ Store.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarClassLibrary
+{
+    public class Store
+    {   
+        //a list of type Car
+        public List<Car> CarList { get; set; }
+        public List<Car> ShoppingList { get; set; }
+
+        public Store() //Constructor method
+        { 
+            CarList = new List<Car>();
+            ShoppingList = new List<Car>();
+        }
+        public decimal Checkout()
+        {
+            //initialize the total cost
+            decimal totalCost = 0;
+
+            foreach (Car car in ShoppingList) 
+            { 
+                totalCost += car.Price;
+            }
+
+            ShoppingList.Clear();
+
+            return totalCost;
+        }
+    }
+}
